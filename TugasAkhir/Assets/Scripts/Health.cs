6a5728ee60f4610d28c90ed8b7ed6ac7f2ae5d0b
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2024-12-26 00:19:52 +0700

    iframes

--- TugasAkhir/Assets/Scripts/Health.cs
+++ TugasAkhir/Assets/Scripts/Health.cs
@@ -5,25 +5,31 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [Header ("Health")]
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
     private Animator anim;
     private bool dead;
 
+    [Header("iFrames")]
+    [SerializeField] private float iFrameDuration;
+    [SerializeField] private int numberOfflashes;
+    private SpriteRenderer spriteRend;
+
     private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
     public void TakeDamage(float _damage)
     {
         currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
-        //player hurt
         if (currentHealth > 0)
         {
             anim.SetTrigger("hurt");
+            StartCoroutine(Invunerability());
         }
-        //player dead
         else
         {
             anim.SetTrigger("die");
@@ -34,5 +40,19 @@ public class Health : MonoBehaviour
     public void AddHealth(float _value)
     {
         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+
+    private IEnumerator Invunerability()
+    {
+        Physics.IgnoreLayerCollision(10, 11, true);
+        for (int i = 0; i < numberOfflashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(1);
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(1);
+
+        }
+        Physics2D.IgnoreLayerCollision(10, 11, false);
     }
 }
